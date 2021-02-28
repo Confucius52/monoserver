285ae4e5176b3546692c9868ca987594fa53d65f
Karthikeya Kona <konakarthik12@gmail.com>
2021-02-28 13:21:26 -0500

    actual commit message

--- src/dice.ts
+++ src/dice.ts
@@ -1,9 +1,8 @@
 import { type, Schema } from "@colyseus/schema"
 
 export class Dice extends Schema {
-	@type("number") first = 0
-	@type("number") second = 0
+    @type("number") first = 0
+    @type("number") second = 0
+    consecutiveDoubles = 0;
 
-	
-	
 }
